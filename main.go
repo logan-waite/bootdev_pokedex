da3574f96b774985ea376ab5a8ff60e0e22a4180
Logan Waite <lwaite2010@gmail.com>
2025-04-13 07:42:42 -0400

    implement repl

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for true {
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			input := scanner.Text()
+			cleaned := cleanInput(input)
+			fmt.Println("Your command was:", cleaned[0])
+		}
+	}
 }
